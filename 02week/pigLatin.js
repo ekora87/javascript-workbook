2a0d028a1880df22017b9349deee3e69eb386f15
ekora87 <ekora87@gmail.com>
2017-07-19 09:36:36 -0500

    Pig Latin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -11,9 +11,26 @@ const rl = readline.createInterface({
 function pigLatin(word) {
 
   // Your code here
-
+  word = word.toLowerCase().trim();
+  if (word[0] === 'a' || word[0] === 'e' || word[0] === 'i' || word[0] === 'o' || word[0] === 'u') {
+    return word + 'yay';
+  }
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var resultsObj = [];
+  for (var i = 0; i < vowels.length; i++) {
+    if (word.indexOf(vowels[i]) > 0) {
+      resultsObj.push(word.indexOf(vowels[i]));
+    }
+  }
+  if (resultsObj.length > 1) {
+    var vowelIndex = Math.min.apply(null, resultsObj);
+  } else {
+    vowelIndex = resultsObj[0];
+  }
+  var firstPart = word.slice(0, vowelIndex);
+  var theRest = word.slice(vowelIndex, word.length);
+  return theRest + firstPart + 'ay';
 }
-
 
 function getPrompt() {
   rl.question('word ', (answer) => {
