3ee6dc5a26c56c15d3db53bbf9e6a6b1845beb02
ekora87 <ekora87@gmail.com>
2017-08-01 16:02:59 -0500

    Algorithms

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -14,12 +14,57 @@ for (let i = 0; i < 1000; i++) {
   arr.push(getRandomInt(0, 1000));
 }
 
+let swapped;
 function bubbleSort(arr) {
   // Your code here
+  do {
+  swapped = false;
+  var end = arr.length - 1;
+  for (var i = 0, j = 1; i < end; i++, j++) {
+    if (arr[i] > arr[j]) {
+      swapped = true;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  } }while (swapped);
+  return arr;
 }
 
-function mergeSort(arr) {
-  // Your code here
+// function mergeSort(arr) {
+//   // Your code here
+//
+// }
+
+function mergeSort(arr)
+{
+    if (arr.length < 2)
+        return arr;
+
+    var middle = Math.floor(arr.length / 2);
+    var left   = arr.slice(0, middle);
+    var right  = arr.slice(middle, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right)
+{
+    var result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
 }
 
 function binarySearch(arr, item) {
