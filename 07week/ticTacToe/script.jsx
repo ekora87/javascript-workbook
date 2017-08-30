e62e11f45a350f35a1db15418640f9b1aecfda86
ekora87 <ekora87@gmail.com>
2017-08-30 18:45:55 -0500

    Towers Of Hanoi React

--- 07week/ticTacToe/script.jsx
+++ 07week/ticTacToe/script.jsx
@@ -3,35 +3,77 @@
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
-  }
+
+  this.board = {
+  0: null,
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null,
+  7: null,
+  8: null
+};
+
+  this.state = {
+    playerTurn : 'X'
+  };
+  this.makeMove = this.makeMove.bind(this);
+  this.checkForWin = this.checkForWin.bind(this);
+}
 
   render() {
-
     return (
       <div>
         <div className="row">
-          <div data-cell="0" onClick={this.makeMove}></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
+          <div data-cell="0" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="1" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="2" ref="cell" onClick={this.makeMove}></div>
         </div>
         <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
+          <div data-cell="3" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="4" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="5" ref="cell" onClick={this.makeMove}></div>
         </div>
         <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
+          <div data-cell="6" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="7" ref="cell" onClick={this.makeMove}></div>
+          <div data-cell="8" ref="cell" onClick={this.makeMove}></div>
         </div>
       </div>
     );
+  }
 
-  }
   makeMove(e) {
-    //debugger;
-    e.target.innerHTML = 'X';
+    e.target.innerHTML = this.state.playerTurn;
+    const val = e.target.getAttribute('data-cell');
+    this.board[val] = this.state.playerTurn;
+    this.checkForWin();
+    this.setState((oldState) => {
+        return oldState.playerTurn = (oldState.playerTurn === 'X') ? 'O' : 'X';
+      });
   }
+
+  checkForWin () {
+    let winCombo = [
+      ['0', '1', '2'],
+      ['3', '4', '5'],
+      ['6', '7', '8'],
+      ['0', '3', '6'],
+      ['1', '4', '7'],
+      ['2', '5', '8'],
+      ['0', '4', '8'],
+      ['2', '4', '6']
+    ];
+    for (let i = 0; i < winCombo.length; i++) {
+        if (this.board[winCombo[i][0]] === this.state.playerTurn &&
+            this.board[winCombo[i][1]] === this.state.playerTurn &&
+            this.board[winCombo[i][2]] === this.state.playerTurn) {
+              setTimeout(function(){ alert("You Won"); }, 300);
+            }
+      }
+    }
 }
 
 ReactDOM.render(<TicTacToe />, document.getElementById('container'));
---
+++ 07week/towersOfHanoi/script.jsx
@@ -0,0 +1,73 @@
+'use strict';
+
+class TowersOfHanoi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.holder = '';
+    this.val = 0;
+    this.compare = 0;
+    this.state = {
+      clicked: false
+    }
+    this.moveBlock = this.moveBlock.bind(this);
+    this.addBlock = this.addBlock.bind(this);
+    this.checkForWin = this.checkForWin.bind(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <div data-stack="1" onClick={this.addBlock}>
+          <div data-block="100" onClick={this.moveBlock}></div>
+          <div data-block="75" onClick={this.moveBlock}></div>
+          <div data-block="50" onClick={this.moveBlock}></div>
+          <div data-block="25" onClick={this.moveBlock}></div>
+        </div>
+        <div data-stack="2" onClick={this.addBlock}>
+        </div>
+        <div data-stack="3" ref="stack3" onClick={this.addBlock}>
+        </div>
+      </div>
+    );
+  }
+
+  moveBlock(e) {
+    const parent = e.target.parentNode;
+    if (e.target === parent.children[parent.children.length -1] && !this.state.clicked) {
+      this.holder = e.target;
+      this.val = e.target.getAttribute('data-block');
+      e.target.remove(e.target);
+      this.setState((state) => {
+        state.clicked = !state.clicked;
+      });
+    }
+  }
+
+  addBlock(e) {
+    const child = e.target.children;
+    if (child.length !== 0) {
+      this.compare = child[child.length-1].getAttribute('data-block');
+    }
+    if (this.state.clicked) {
+      if (parseInt(this.compare) > parseInt(this.val) || !this.compare) {
+        e.target.appendChild(this.holder);
+        this.setState((state) => {
+          state.clicked = !state.clicked;
+        });
+        this.compare = 0;
+      } else {
+        alert ("You can't click here. Please pick a different stack");
+        this.compare = 0;
+      }
+    }
+    this.checkForWin();
+  }
+
+  checkForWin () {
+    if (this.refs.stack3.children.length === 4) {
+      setTimeout(function() { alert("You Won"); }, 300);
+    }
+  }
+}
+
+ReactDOM.render(<TowersOfHanoi />, document.getElementById('towers-of-hanoi'));
